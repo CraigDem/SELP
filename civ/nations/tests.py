5a7494aade8e3214242f4b1006ca2fdc2de228ac
CraigDem <craig.peden.mail@gmail.com>
2014-12-17 08:04:29 +0000

    Adding templates to tests file, added report
    Also cleared unneeded planning files that were obsolete or incorrect.

--- civ/nations/tests.py
+++ civ/nations/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from nations import views
 
 # Create your tests here.
+
+print "hello"
+
+# class indexView(View):
+
+# class noEntry(View):
+
+# class registerView(View):
+
+# class nationView(DetailView):
+
+# class editNationView(UpdateView, ModelFormMixin):
+
+# class expandNationView(UpdateView, ModelFormMixin):
+
+# 	def calculate_cost(self,level,amount):
+
+# class billsNationView(DetailView):
+
+# 	def infrastructure_upkeep(self,infrastructure):
+
+#	def calculate_discounts(self, bills, resource1, resource2):
+
+#	def bills_validation(self, nation, date): 
+
+# 	def costs_generator(self, nation):
+
+# class taxesNationView(DetailView):
+
+#	def taxes_validation(self, nation, date): 
+
+#	def calculate_taxes(self,nation):
+
+# class rankNationView(DetailView):
